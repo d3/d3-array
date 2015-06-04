044554332fb7c2079b4516daade4ba406376eb7c
Mike Bostock <mbostock@gmail.com>
2015-06-04 08:42:34 -0700

    Add tests for bisect, bisectLeft and bisectRight.

--- src/bisect.js
+++ src/bisect.js
@@ -2,6 +2,6 @@ import ascending from "./ascending";
 import bisector from "./bisector";
 
 var ascendingBisect = bisector(ascending);
-export default ascendingBisect.right;
 export var bisectRight = ascendingBisect.right;
 export var bisectLeft = ascendingBisect.left;
+export default bisectRight;
