baa683a09d85c8e40c67c6fac2e9ced424fc6407
Jeff Coukell <jeffcoukell@gmail.com>
2015-12-01 11:41:18 -0800

    Added initial implementation for skewness and kurtosis functions.

--- src/kurtosis.js
+++ src/kurtosis.js
@@ -1,5 +1,36 @@
 import number from "./number";
+import mean from "./mean";
 
 export default function(array) {
-  return 0;
+  var m = mean(array),
+      sum4 = 0,
+      sum2 = 0,
+      v,
+      i = -1,
+      n = array.length;
+
+  if (arguments.length === 1) {
+    while (++i < n) {
+      if (!isNaN(a = number(array[i]))) {
+        v = a - m;
+        sum2 += v * v;
+        sum4 += v * v * v * v;
+        j++;
+      }
+    }
+  }
+  
+  else {
+    while (++i < n) {
+      if (!isNaN(a = number(f(array[i], i, array)))) {
+        v = a - m;
+        sum2 += v * v;
+        sum4 += v * v * v * v;
+        j++;
+      }
+    }
+  }
+
+  if (j > 3) return (n * (n + 1) * (n - 1) * sum4) / ((n - 2) * (n - 3) * sum2 * sum2) -
+                    (3 * Math.pow(n - 1, 2) / ((n - 2) * (n - 3)));
 };
--- src/skewness.js
+++ src/skewness.js
@@ -1,5 +1,34 @@
 import number from "./number";
+import mean from "./mean";
 
 export default function(array) {
-  return 0;
+  var m = mean(array),
+      sum3 = 0,
+      sum2 = 0,
+      v,
+      i = -1,
+      j = 0,
+      n = array.length;
+
+  if (arguments.length === 1) {
+    while (++i < n) {
+      if (!isNaN(a = number(array[i]))) {
+        v = a - m;
+        sum2 += v * v;
+        sum3 += v * v * v;
+      }
+    }
+  }
+  
+  else {
+    while (++i < n) {
+      if (!isNaN(a = number(f(array[i], i, array)))) {
+        v = a - m;
+        sum2 += v * v;
+        sum3 += v * v * v;
+      }
+    }
+  }
+
+  return (n * Math.sqrt(n - 1) / (n - 2)) * (sum3 / Math.pow(sum2, 3 / 2)));
 };
