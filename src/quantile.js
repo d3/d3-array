ac7e34691531a906e6ed45bd32490a54a311fdec
Mike Bostock <mbostock@gmail.com>
2015-12-08 13:51:45 -0800

    Optimize.

--- src/quantile.js
+++ src/quantile.js
@@ -6,9 +6,9 @@ export default function(array, p, f) {
   if ((p = +p) <= 0 || n < 2) return +f(array[0], 0, array);
   if (p >= 1) return +f(array[n - 1], n - 1, array);
   var n,
-      h = (n - 1) * p + 1,
+      h = (n - 1) * p,
       i = Math.floor(h),
-      a = +f(array[i - 1], i - 1, array),
-      b = +f(array[i], i, array);
+      a = +f(array[i], i, array),
+      b = +f(array[i + 1], i + 1, array);
   return a + (b - a) * (h - i);
 };
