1651af2cf25ce5a66b4eba7e1fa46fda792091f7
Mike Bostock <mbostock@gmail.com>
2016-01-12 14:01:14 -0800

    Revert part of 8b9e322046b4f859ac9898126193fee22412ff64.
    Related mbostock/d3#2710.

--- src/range.js
+++ src/range.js
@@ -1,14 +1,14 @@
 export default function(start, stop, step) {
-  step = step == null ? 1 : +step;
-  if (stop == null) stop = +start, start = 0;
-  else start = +start, stop = +stop;
-
-  var i = -1,
-      n = Math.max(0, Math.ceil((stop - start) / step)) | 0,
+  var m = arguments.length,
+      start_ = +start,
+      stop_ = +stop,
+      step_ = m < 2 ? (stop_ = start_, start_ = 0, 1) : m < 3 ? 1 : +step,
+      i = -1,
+      n = Math.max(0, Math.ceil((stop_ - start_) / step_)) | 0,
       range = new Array(n);
 
   while (++i < n) {
-    range[i] = start + i * step;
+    range[i] = start_ + i * step_;
   }
 
   return range;
