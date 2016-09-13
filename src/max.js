3c6718b0fec28492d29daeca481e6fcdbbbfbd18
geekplux <geekplux@qq.com>
2016-09-13 16:07:42 +0800

    refactor max by accessor

--- src/max.js
+++ src/max.js
@@ -1,18 +1,13 @@
+import accessor from './accessor';
+
 export default function(array, f) {
   var i = -1,
       n = array.length,
       a,
       b;
 
-  if (f == null) {
-    while (++i < n) if ((b = array[i]) != null && b >= b) { a = b; break; }
-    while (++i < n) if ((b = array[i]) != null && b > a) a = b;
-  }
-
-  else {
-    while (++i < n) if ((b = f(array[i], i, array)) != null && b >= b) { a = b; break; }
-    while (++i < n) if ((b = f(array[i], i, array)) != null && b > a) a = b;
-  }
+  while (++i < n) if ((b = accessor(f, i, array)) != null && b >= b) { a = b; break; }
+  while (++i < n) if ((b = accessor(f, i, array)) != null && b > a) a = b;
 
   return a;
 }
