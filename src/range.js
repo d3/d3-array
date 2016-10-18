88673427850c51d5a1a4c26504a50db41e50814c
Tyler Breisacher <tbreisacher@google.com>
2016-10-18 13:43:56 -0700

    formatting fixes

--- src/range.js
+++ src/range.js
@@ -1,8 +1,6 @@
 export default function(start, stop, step) {
   var n;
-  start = +start, stop = +stop, step = (n = arguments.length) < 2 ?
-      (stop = start, start = 0, 1) :
-      n < 3 ? 1 : +step;
+  start = +start, stop = +stop, step = (n = arguments.length) < 2 ? (stop = start, start = 0, 1) : n < 3 ? 1 : +step;
 
   var i = -1;
   n = Math.max(0, Math.ceil((stop - start) / step)) | 0;
