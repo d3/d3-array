695ff15386b6763ccfc7fe93900e22295dd21bbd
Mike Bostock <mbostock@gmail.com>
2018-08-24 11:01:10 -0700

    Update dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,12 @@ const config = {
   input: "index.js",
   external: Object.keys(meta.dependencies || {}),
   output: {
-    file: `build/${meta.name}.js`,
+    file: `dist/${meta.name}.js`,
     name: "d3",
     format: "umd",
+    indent: false,
     extend: true,
-    banner: `// ${meta.homepage} Version ${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}.`,
+    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
     globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).map(key => ({[key]: "d3"})))
   },
   plugins: []
@@ -21,7 +22,7 @@ export default [
     ...config,
     output: {
       ...config.output,
-      file: `build/${meta.name}.min.js`
+      file: `dist/${meta.name}.min.js`
     },
     plugins: [
       ...config.plugins,
