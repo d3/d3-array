06a41074cee6cea7f9ddb99fb990598d991491c2
Mike Bostock <mbostock@gmail.com>
2018-11-10 13:13:34 -0800

    Iterable cross.

--- src/cross.js
+++ src/cross.js
@@ -1,21 +1,22 @@
-import {pair} from "./pairs";
-
-export default function(values0, values1, reduce) {
-  var n0 = values0.length,
-      n1 = values1.length,
-      values = new Array(n0 * n1),
-      i0,
-      i1,
-      i,
-      value0;
-
-  if (reduce == null) reduce = pair;
-
-  for (i0 = i = 0; i0 < n0; ++i0) {
-    for (value0 = values0[i0], i1 = 0; i1 < n1; ++i1, ++i) {
-      values[i] = reduce(value0, values1[i1]);
+function* product(head, ...tail) {
+  if (tail.length) {
+    for (const h of head) {
+      for (const t of product(...tail)) {
+        yield [h, ...t];
+      }
+    }
+  } else {
+    for (const h of head) {
+      yield [h];
     }
   }
+}
 
-  return values;
+function reducer(reduce) {
+  return values => reduce(...values);
+}
+
+export default function cross(...values) {
+  const reduce = typeof values[values.length - 1] === "function" ? reducer(values.pop()) : undefined;
+  return Array.from(product(...values), reduce);
 }
--- test/cross-test.js
+++ test/cross-test.js
@@ -6,7 +6,30 @@ tape("cross(a, b) returns Cartesian product a×b", function(test) {
   test.end();
 });
 
+tape("cross(a, b, c) returns Cartesian product a×b×c", function(test) {
+  test.deepEqual(arrays.cross([1, 2], [3, 4], [5, 6, 7]), [
+    [1, 3, 5],
+    [1, 3, 6],
+    [1, 3, 7],
+    [1, 4, 5],
+    [1, 4, 6],
+    [1, 4, 7],
+    [2, 3, 5],
+    [2, 3, 6],
+    [2, 3, 7],
+    [2, 4, 5],
+    [2, 4, 6],
+    [2, 4, 7]
+  ]);
+  test.end();
+});
+
 tape("cross(a, b, f) invokes the specified function for each pair", function(test) {
   test.deepEqual(arrays.cross([1, 2], ["x", "y"], (a, b) => a + b), ["1x", "1y", "2x", "2y"]);
+  test.end();
+});
+
+tape("cross(a, b, c, f) invokes the specified function for each triple", function(test) {
+  test.deepEqual(arrays.cross([1, 2], [3, 4], [5, 6, 7], (a, b, c) => a + b + c), [9, 10, 11, 10, 11, 12, 10, 11, 12, 11, 12, 13]);
   test.end();
 });
