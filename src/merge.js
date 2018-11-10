8502fa0dad5e7afafc6e1387750136e76013c2e1
Mike Bostock <mbostock@gmail.com>
2018-11-10 13:18:57 -0800

    Iterable merge.

--- src/merge.js
+++ src/merge.js
@@ -1,21 +1,9 @@
-export default function(arrays) {
-  var n = arrays.length,
-      m,
-      i = -1,
-      j = 0,
-      merged,
-      array;
-
-  while (++i < n) j += arrays[i].length;
-  merged = new Array(j);
-
-  while (--n >= 0) {
-    array = arrays[n];
-    m = array.length;
-    while (--m >= 0) {
-      merged[--j] = array[m];
-    }
+function* flatten(arrays) {
+  for (const array of arrays) {
+    yield* array;
   }
+}
 
-  return merged;
+export default function merge(arrays) {
+  return Array.from(flatten(arrays));
 }
