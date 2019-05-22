7d94aeb051f4c39d95c3da5b979a56793501cf8b
Mike Bostock <mbostock@gmail.com>
2019-05-22 11:59:19 -0700

    Use minIndex for leastIndex.

--- src/leastIndex.js
+++ src/leastIndex.js
@@ -1,30 +1,19 @@
 import ascending from "./ascending.js";
+import minIndex from "./minIndex.js";
 
 export default function leastIndex(values, compare = ascending) {
-  let min;
-  let minIndex = -1;
+  if (compare.length === 1) return minIndex(values, compare);
+  let minValue;
+  let min = -1;
   let index = -1;
-  if (compare.length === 1) {
-    for (const element of values) {
-      ++index;
-      const value = compare(element);
-      if (minIndex < 0
-          ? ascending(value, value) === 0
-          : ascending(value, min) < 0) {
-        min = value;
-        minIndex = index;
-      }
-    }
-  } else {
-    for (const value of values) {
-      ++index;
-      if (minIndex < 0
-          ? compare(value, value) === 0
-          : compare(value, min) < 0) {
-        min = value;
-        minIndex = index;
-      }
+  for (const value of values) {
+    ++index;
+    if (min < 0
+        ? compare(value, value) === 0
+        : compare(value, minValue) < 0) {
+      minValue = value;
+      min = index;
     }
   }
-  return minIndex;
+  return min;
 }
