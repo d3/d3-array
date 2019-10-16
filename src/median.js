6ebf2c2a21edab9ae261ea433b710712ef010311
Philippe Rivière <fil@rezo.net>
2019-10-16 22:12:15 +0200

    use quantile(.5) for median alternative to #125

--- src/median.js
+++ src/median.js
@@ -1,13 +1,5 @@
-import {numbers} from "./number.js";
 import quantile from "./quantile.js";
-import quickselect from "./quickselect.js";
 
 export default function(values, valueof) {
-  values = Float64Array.from(numbers(values, valueof));
-  if (!values.length) return;
-  const n = values.length;
-  const i = n >> 1;
-  quickselect(values, i - 1, 0);
-  if ((n & 1) === 0) quickselect(values, i, i);
-  return quantile(values, 0.5);
+  return quantile(values, 0.5, valueof);
 }
