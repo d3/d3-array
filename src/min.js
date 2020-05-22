8da858f3e28a36cfe336da61c68b37f0665f59de
MrHeer <hlm52pk@163.com>
2020-05-22 14:53:38 +0800

    refactor: simplify code

--- src/min.js
+++ src/min.js
@@ -1,19 +1,13 @@
 export default function min(values, valueof) {
   let min;
   if (valueof === undefined) {
-    for (const value of values) {
-      if (value != null
-          && (min > value || (min === undefined && value >= value))) {
-        min = value;
-      }
-    }
-  } else {
-    let index = -1;
-    for (let value of values) {
-      if ((value = valueof(value, ++index, values)) != null
-          && (min > value || (min === undefined && value >= value))) {
-        min = value;
-      }
+    valueof = (value) => value;
+  }
+  let index = -1;
+  for (let value of values) {
+    if ((value = valueof(value, ++index, values)) != null
+      && (min > value || (min === undefined && value >= value))) {
+      min = value;
     }
   }
   return min;
