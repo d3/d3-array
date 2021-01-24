d3e44630b851816f24c0757dd81bbfb841ca0899
Mike Bostock <mbostock@gmail.com>
2021-01-23 17:12:16 -0800

    add maxSort, minSort

--- src/groupSort.js
+++ src/groupSort.js
@@ -1,6 +1,8 @@
 import ascending from "./ascending.js";
 import group, {rollup} from "./group.js";
+import max from "./max.js";
 import median from "./median.js";
+import min from "./min.js";
 import sort from "./sort.js";
 
 export default function groupSort(values, reduce, key) {
@@ -10,6 +12,14 @@ export default function groupSort(values, reduce, key) {
     .map(([key]) => key);
 }
 
+export function maxSort(values, valueof, key) {
+  return groupSort(values, group => max(group, valueof), key);
+}
+
 export function medianSort(values, valueof, key) {
   return groupSort(values, group => median(group, valueof), key);
+}
+
+export function minSort(values, valueof, key) {
+  return groupSort(values, group => min(group, valueof), key);
 }
