c172c47fb68bfee2c0b519ac4d4a7884a59721c2
Mike Bostock <mbostock@gmail.com>
2021-06-05 08:44:32 -0700

    extract tests

--- test/group-test.js
+++ test/group-test.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import {group, flatGroup, flatRollup} from "../src/index.js";
+import {group} from "../src/index.js";
 
 const data = [
   {name: "jim",   amount: "34.0",   date: "11/12/2015"},
@@ -127,41 +127,6 @@ it("group(data, accessor) interns keys", () => {
   assert.deepStrictEqual(map.get(+b), [[b, 3]]);
   assert.strictEqual([...map.keys()][0], a1);
   assert.strictEqual([...map.keys()][1], b);
-});
-
-it("flatGroup(data, accessor, accessor) returns the expected array", () => {
-  assert.deepStrictEqual(
-    flatGroup(data, d => d.name, d => d.amount),
-    [
-      ['jim', '34.0', [{name: 'jim', amount: '34.0', date: '11/12/2015'}]],
-      ['carl', '120.11', [{name: 'carl', amount: '120.11', date: '11/12/2015'}]],
-      ['stacy', '12.01', [{name: 'stacy', amount: '12.01', date: '01/04/2016'}]],
-      ['stacy', '34.05', [{name: 'stacy', amount: '34.05', date: '01/04/2016'}]]
-    ]
-  );
-});
-
-it("flatRollup(data, reduce, accessor) returns the expected array", () => {
-  assert.deepStrictEqual(
-    flatRollup(data, v => v.length, d => d.name),
-    [
-      ['jim', 1],
-      ['carl', 1],
-      ['stacy', 2]
-    ]
-  );
-});
-
-it("flatRollup(data, reduce, accessor, accessor) returns the expected array", () => {
-  assert.deepStrictEqual(
-    flatRollup(data, v => v.length, d => d.name, d => d.amount),
-    [
-      ['jim', '34.0', 1],
-      ['carl', '120.11', 1],
-      ['stacy', '12.01', 1],
-      ['stacy', '34.05', 1]
-    ]
-  );
 });
 
 function entries(map, depth) {
