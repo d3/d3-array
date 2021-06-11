2b74ffc272877a09615d5e36e8a5080b5976a253
Thomas Zimmermann <tom@Thomass-MacBook-Pro.local>
2021-06-11 16:03:44 +0200

    syntax was breaking our app build

--- src/cumsum.js
+++ src/cumsum.js
@@ -1,6 +1,7 @@
 export default function cumsum(values, valueof) {
+  var fn = v => (sum += +valueof(v, index++, values) || 0);
+  if (valueof === undefined)
+    fn = v => (sum += +v || 0);
   var sum = 0, index = 0;
-  return Float64Array.from(values, valueof === undefined
-    ? v => (sum += +v || 0)
-    : v => (sum += +valueof(v, index++, values) || 0));
+  return Float64Array.from(values, fn);
 }
