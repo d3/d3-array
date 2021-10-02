d3d0198f8f5c233034b2034f4a5a49950a149b9d
Mike Bostock <mbostock@gmail.com>
2021-10-01 21:42:58 -0700

    allow rank to take a comparator

--- src/rank.js
+++ src/rank.js
@@ -1,23 +1,31 @@
-import range from "./range.js";
-import sort from "./sort.js";
+import {ascendingDefined, compareDefined} from "./sort.js";
 
 export default function rank(values, valueof) {
   if (typeof values[Symbol.iterator] !== "function") throw new TypeError("values is not iterable");
-  values = Array.from(values, valueof);
-  const n = values.length;
-  const r = new Float64Array(n);
-  let last, l;
-  sort(range(n), (i) => values[i]).forEach((j, i) => {
-    const value = values[j];
-    if (value == null || !(value <= value)) {
-      r[j] = NaN;
+  const V = Array.from(values);
+  const R = new Float64Array(V.length);
+  const compare = valueof === undefined ? ascendingIndex(V)
+      : valueof.length === 1 ? ascendingIndex(V.map(valueof))
+      : compareIndex(V, valueof);
+  let k, r;
+  Uint32Array.from(V, (_, i) => i).sort(compare).forEach((j, i) => {
+    if (compare(j, j) !== 0) {
+      R[j] = NaN;
       return;
     }
-    if (last === undefined || !(value <= last)) {
-      last = value;
-      l = i;
+    if (k === undefined || !(compare(j, k) <= 0)) {
+      k = j;
+      r = i;
     }
-    r[j] = l;
+    R[j] = r;
   });
-  return r;
+  return R;
+}
+
+function ascendingIndex(V) {
+  return (i, j) => ascendingDefined(V[i], V[j]);
+}
+
+function compareIndex(V, compare) {
+  return compareDefined((i, j) => compare(V[i], V[j]));
 }
