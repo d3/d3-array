57225fc08cdcc6c53b0eca5a37774d95241b326f
Mike Bostock <mbostock@gmail.com>
2022-04-03 10:41:17 -0500

    fix for quantization error with fractional step (#247)
    * fix for quantization error with fractional step
    
    * conditional fix for rounding error

--- test/bin-test.js
+++ test/bin-test.js
@@ -1,5 +1,7 @@
 import assert from "assert";
-import {bin, extent, histogram, thresholdSturges} from "../src/index.js";
+import {csvParse} from "d3-dsv";
+import {readFile} from "fs/promises";
+import {bin, extent, histogram, thresholdSturges, ticks} from "../src/index.js";
 
 it("histogram is a deprecated alias for bin", () => {
   assert.strictEqual(histogram, bin);
@@ -219,6 +221,18 @@ it("bin(data) coerces values to numbers as expected", () => {
     box([], 4.5, 5),
     box(["5"], 5, 5.5)
   ]);
+});
+
+it("bin(athletes) produces the expected result", async () => {
+  const height = csvParse(await readFile("./test/data/athletes.csv", "utf8")).filter(d => d.height).map(d => +d.height);
+  const bins = bin().thresholds(57)(height);
+  assert.deepStrictEqual(bins.map(b => b.length), [1, 0, 0, 0, 0, 0, 2, 1, 2, 1, 1, 4, 11, 7, 13, 39, 78, 93, 119, 193, 354, 393, 573, 483, 651, 834, 808, 763, 627, 648, 833, 672, 578, 498, 395, 425, 278, 235, 182, 128, 91, 69, 43, 29, 21, 23, 3, 3, 1, 1, 1]);
+});
+
+it("bin(data) assigns floating point values to the correct bins", () => {
+  for (const n of [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]) {
+    assert.ok(bin().thresholds(n)(ticks(1, 2, n)).every(d => d.length === 1));
+  }
 });
 
 function box(bin, x0, x1)  {
