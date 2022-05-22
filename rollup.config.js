d9fb1a5d1497dc2908c1606442c7d7507b0d2511
Kris Dages <krisdages@git.whiteboxsoftware.net>
2022-05-22 13:54:39 -0500

    add minified cjs output with source map, use cjs for `main` and `exports.require`
    Allows package to be used with both `import` and `require`

--- rollup.config.js
+++ rollup.config.js
@@ -49,5 +49,28 @@ export default [
         }
       })
     ]
+  },
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: `dist/${meta.name}.min.cjs`,
+      format: "cjs",
+      sourcemap: true
+    },
+    plugins: [
+      ...config.plugins,
+      terser({
+        output: {
+          preamble: config.output.banner
+        },
+        mangle: {
+          reserved: [
+            "InternMap",
+            "InternSet"
+          ]
+        }
+      })
+    ]
   }
 ];
