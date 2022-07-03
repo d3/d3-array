aa6defefc6fe33d5a8f37767c2839aa6e3eeb577
Mike Bostock <mbostock@gmail.com>
2022-07-03 08:17:27 -0400

    match style

--- src/linspace.js
+++ src/linspace.js
@@ -1,10 +1,12 @@
 export default function(start, stop, n = 50, endpoint = true) {
-  n = Math.floor(n);
-  start = +start;
-  stop = (stop - start) / (endpoint ? n - 1 : n);
+  n = Math.floor(n), start = +start, stop = (stop - start) / (endpoint ? n - 1 : n);
 
-  const range = new Array(n);
-  for (let i = 0; i < n; ++i) range[i] = start + i * stop;
+  var i = -1,
+      range = new Array(n);
+
+  while (++i < n) {
+    range[i] = start + i * stop;
+  }
 
   return range;
 }
